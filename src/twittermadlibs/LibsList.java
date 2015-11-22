d62840822ad3e8c250f95ee14706b8f0646ccfa9
Justin Hill <jhillkwaj@gmail.com>
2015-11-22 10:05:12 -0600

    Added two more libs

--- src/twittermadlibs/LibsList.java
+++ src/twittermadlibs/LibsList.java
@@ -7,7 +7,7 @@ package twittermadlibs;
 
 /**
  *
- * @author justi
+ * @author justin
  */
 public class LibsList {
 	public static String randomLib() {
@@ -21,7 +21,9 @@ public class LibsList {
 			"Two <plural noun>, both alike in dignity, In fair <noun>c, where we lay our scene,From ancient <noun> break to new mutiny, Where civil blood makes civil hands unclean. From forth the fatal loins of these two foes A pair of star-cross`d <plural noun> take their life; Whole misadventured piteous overthrows Do with their <noun> bury their parents` strife. The fearful passage of their <adjective> love, And the continuance of their parents` rage, Which, but their children`s end, nought could <verb>, Is now the <number> hours` traffic of our stage; The which if you with <adjective> <noun> attend, What here shall <verb>, our toil shall strive to mend.",
 			"Is this the real life? Is this just <noun>? Caught in a <noun> No escape from <noun>. Open your <plural noun> <verb> up to the <plural noun> and <verb>. I'm just a <adjective> boy, I need no sympathy Because I'm <adjective> come, <adjective> go A <adverb> high, <adverb> low. Anyway the <noun> <plural verb>, doesn't <adverb> <verb> to <noun>, to <noun>.",
 			"I am Dr. <adjective>c. I am destined to <verb> the <noun>. Unfortunately, the <noun> is full of a bunch of <adjective> <plural noun> that do not agree. I am sad to say that the <noun> Buster did not trigger the huge <adjective> as I planned for it to do. Instead, all it did was <verb> some <plural noun>. So this is my new <adjective> plan to take over the <noun>: Using my unlimited source of <plural noun> I inherited from my <plural proper noun> I will build the largest <noun> in the <noun>. I will then send it up into space aboard a <noun>. It will be fully <adjective> and operable from a <adjective> control. With it I will threaten to <verb> all the <plural noun> owned by <proper noun> unless the <noun> bows before me. If they do not <verb> me, I will turn my <adjective> upon the <proper noun> islands and <verb> the entire thing. Then they will <verb> in their <plural noun> and I will be the most powerful <noun> in the <noun>!",
-			"<plural noun>c are the most <adjective> <plural noun> used by mankind. Teamwork is the ability to <verb> together towards a common <noun>. It is the fule that <verb> <adjective> people to attain un-<verb> results. I am more afraid of an army of <number> <plural noun> led by a <noun> than an army of <number> <plural noun> led by a <noun>."
+			"All around the <adjective> bush The <noun> <past verb> the <noun>. The <noun> <past verb> 'twas all in fun. Pop! goes the <noun>.",
+			"<plural noun>c are the most <adjective> <plural noun> used by mankind. Teamwork is the ability to <verb> together towards a common <noun>. It is the fule that <verb> <adjective> people to attain un-<verb> results. I am more afraid of an army of <number> <plural noun> led by a <noun> than an army of <number> <plural noun> led by a <noun>.",
+			"Robert Forester was a <adjective> man. He was alway a <adjective> type of man. He never hurt anyone... Until <number> years ago. He was even the one who killed <proper noun>! He used different things to murder people with. Even <plural noun>! He once even shoved a man's <noun> into a <noun>. He was inspired by <proper noun>. He took souveniors from his victoms, such as <plural noun>. He then was caught by a <adjective> police man because of the <adjective> smell coming from his <noun>. He had over <number> bodies in his <noun>. He was sent to prison for <number> life terms."
 
 };
 }
